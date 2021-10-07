72b3dc1f62f909d3bcbb35075d2d90e1788a5e97
robin hardjowijono <robinhardjowijono@hotmail.com>
2021-10-07 20:00:03 +0200

    Helemaal klaar en af

--- main.js
+++ main.js
@@ -1,35 +1,9 @@
-// var clicks = 0;
-
-// function clicker() {
-//     clicks += 1;
-//     document.getElementById("clicker").innerHTML = clicks
-// }
-
-// var clicks2 = 0;
-
-// function clicker2() {
-//     clicks2 += 1;
-//     document.getElementById("clicker2").innerHTML = clicks2
-// }
-
-// // Timer
-// var time = 5;
-// var timer = setInterval(function(){
-//  time--;
-//  document.getElementById("timer").innerHTML = time + "s";
-//  if(time==0){
-//    clearInterval(timer);
-//    document.getElementById("timer").innerHTML = "Time's up!";
-//    outOfTime = true;
-//  }
-// }, 1000);
-
-let elementTimer = document.getElementById("timer")
-let elementSpelIsActief = document.getElementById("spelIsActief")
-let elementScore1 = document.getElementById("score1")
-let elementScore2 = document.getElementById("score2")
-let elementWinner = document.getElementById("winner")
-let elementReset = document.getElementById("reset")
+let elementTimer = document.getElementById("timer");
+let elementSpelIsActief = document.getElementById("spelIsActief");
+let elementScore1 = document.getElementById("score1");
+let elementScore2 = document.getElementById("score2");
+let elementWinner = document.getElementById("winner");
+let elementReset = document.getElementById("reset");
 
 let timer = 6;
 
@@ -42,16 +16,57 @@ let player1Score = 0;
 let player2Score = 0;
 
 function countDown() {
-  if (timer == 0){
+  elementScore1.innerHTML = player1Score;
+  elementScore2.innerHTML = player2Score;
+  clickAllowed = false;
+  if (timer == 0) {
     gameStart();
+  } else {
+    timer = timer - 1;
+    elementTimer.innerHTML = timer;
+    setTimeout(countDown, 1000);
   }
-  timer = timer-1;
-  elementTimer.innerHTML = timer;
-  setTimeout(countDown,1000)
 }
 
 function gameStart() {
+  timer = 10;
+  elementTimer.innerHTML = timer;
   clickAllowed = true;
+  setTimeout(gameCountDown, 1000);
 }
 
-countDown()
+document.addEventListener("keydown", function (event) {
+  if (event.key == "a") {
+    if (clickAllowed == true) {
+      player1Score = player1Score + 1;
+      elementScore1.innerHTML = player1Score;
+    }
+  }
+  if (event.key == "l") {
+    if (clickAllowed == true) {
+      player2Score = player2Score + 1;
+      elementScore2.innerHTML = player2Score;
+    }
+  }
+});
+
+function gameCountDown() {
+  if (timer == 0) {
+    clickAllowed = false;
+    if (player1Score == player2Score) {
+      elementWinner.innerHTML = "Het is gelijkspel";
+    } else {
+      if (player1Score > player2Score) {
+        elementWinner.innerHTML = "Player 1 heeft gewonnen";
+      } else {
+        elementWinner.innerHTML = "Player 2 heeft gewonnen";
+      }
+    }
+  } else {
+    timer = timer - 1;
+    elementTimer.innerHTML = timer;
+    setTimeout(gameCountDown, 1000);
+  }
+}
+
+countDown();
